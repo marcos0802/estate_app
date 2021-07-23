a5427fe4cbfaadf8e4c6b41ba2ca642338e06bca
root <root@localhost.localdomain>
2021-07-23 20:46:10 +0100

    start - assets - data

---
+++ lib/data/data.dart
@@ -0,0 +1,165 @@
+class Property {
+
+  String label;
+  String name;
+  String price;
+  String location;
+  String sqm;
+  String review;
+  String description;
+  String frontImage;
+  String ownerImage;
+  List<String> images;
+
+  Property(this.label, this.name, this.price, this.location, this.sqm, this.review, this.description, this.frontImage, this.ownerImage, this.images);
+
+}
+
+List<Property> getPropertyList(){
+  return <Property>[
+    Property(
+      "SALE",
+      "Marcos Musafiri",
+      "3,500.00",
+      "Kinshasa",
+      "2,456",
+      "4.4",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_01.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "RENT",
+      "Joseph Kiserbo",
+      "3,500.00",
+      "L'shi",
+      "3,300",
+      "4.6",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_04.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "RENT",
+      "Aristide Byamungu",
+      "3,100.00",
+      "Goma",
+      "2,100",
+      "4.1",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_02.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "SALE",
+      "Julienne Heri",
+      "4,500.00",
+      "Goma",
+      "4,100",
+      "4.5",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_03.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "SALE",
+      "Merveille Marie",
+      "5,200.00",
+      "Kinshasa",
+      "3,100",
+      "4.2",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_05.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "SALE",
+      "Marcos Mus",
+      "3,500.00",
+      "Kananga",
+      "3,700",
+      "4.0",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_06.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "RENT",
+      "Emmanuel Kamara",
+      "2,900.00",
+      "Goma",
+      "2,700",
+      "4.3",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_07.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+    Property(
+      "RENT",
+      "Saidati Saidi",
+      "3,900.00",
+      "Kinshasa",
+      "3,700",
+      "4.4",
+      "The living is easy in this impressive, generously proportioned contemporary residence with lake and ocean views, located within a level stroll to the sand and surf.",
+      "assets/images/house_08.jpg",
+      "assets/images/owner.jpg",
+      [
+        "assets/images/kitchen.jpg",
+        "assets/images/bath_room.jpg",
+        "assets/images/swimming_pool.jpg",
+        "assets/images/bed_room.jpg",
+        "assets/images/living_room.jpg",
+      ],
+    ),
+  ];
+}
